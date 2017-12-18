f276278ed1842717e728ac769587cb1b86232906
Mamoriaii <codenya@163.com>
2017-12-18 11:45:31 +0800

    counter and mouseEvent

--- counter.js
+++ counter.js
@@ -1,8 +1,19 @@
 
 var c=0
-var t
+var t;
+var cnt = 0;
+function togle(){
+	if(!cnt){
+		timedCount();
+		cnt = 1;
+	}else {
+		stopCount();
+		cnt = 0;
+	}
+}
 function timedCount()
 {
+	document.getElementById('btn').value="停止计时！";
 document.getElementById('txt').value=c
 c=c+1
 t=setTimeout("timedCount()",1000)
@@ -11,6 +22,7 @@ t=setTimeout("timedCount()",1000)
 function stopCount()
 {
 c=0;
+document.getElementById('btn').value="开始计时！";
 setTimeout("document.getElementById('txt').value=0",0);
 clearTimeout(t);
 }
